9328ec91d731278d48e61e2295ebdfbd314fc483
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-09-26 01:52:44 +0500

    helloworld

---
+++ helloworld/src/pages/index.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div>
+     <h1 className="text-4xl text-center mt-6 text-white font-semibold"> 
+      Hello World! 
+      </h1>
+    </div>
+  )
+}
